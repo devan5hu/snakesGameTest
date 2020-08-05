d2a4f83d4e5b8cc87ef23898eb77ae4e36c8bdf3
Devanshu Chaudhari <devanshuchaudhari4@gmail.com>
2020-08-05 14:42:38 +0530

    smake moving but not eating food in the second loop.

--- snake.js
+++ snake.js
@@ -1,15 +1,19 @@
-export const SNAKE_SPEED=1
+import { GetInputDirections } from "./input.js"
+export const SNAKE_SPEED=5
 const snakeBody = [{x:11,y:11}]
+let newSegments=0
 
 
 export function update()
 {
-    for(let i= snakeBody.length-2;i>=0;i++)
+    addSegments()
+    const inputDirection=GetInputDirections()
+    for(let i= snakeBody.length-2;i>=0;i--)
     {
         snakeBody[i+1]={...snakeBody[i]}
     }
-    // snakeBody[0].x +=1;
-    // snakeBody[0].y +=0;
+    snakeBody[0].x +=inputDirection.x;
+     snakeBody[0].y +=inputDirection.y;
 
 
 }
@@ -23,4 +27,28 @@ export function draw(gameBoard)
             SnakeElement.classList.add('snake')
             gameBoard.appendChild(SnakeElement)
         })
+}
+
+export function expandSnake(amount)
+{
+    newSegments += amount;
+}
+export function OnSnake(position)
+{
+    return snakeBody.some(segment => {
+        return equalPositions(segment,position)
+    })
+}
+
+function equalPositions (pos1,pos2)
+{
+    return pos1.x==pos2.x && pos1.y==pos2.y
+}
+
+function addSegments(){
+    for(let i=0;i<newSegments;i++)
+    {
+        snakeBody.push({...snakeBody[snakeBody.length - 1]})
+    }
+    newSegments=0
 }
